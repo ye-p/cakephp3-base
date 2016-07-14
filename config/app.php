25006b5711f64997f7d347733e3024f77a397deb
奥村 遼平 <okumura@ye-p.co.jp>
2016-07-14 11:25:26 +0900

    コンフィグ修正

--- config/app.php
+++ config/app.php
@@ -228,7 +228,7 @@ return [
             //'port' => 'non_standard_port_number',
             'username' => 'root',
             'password' => 'root',
-            'database' => 'looop_rj',
+            'database' => 'sample_db',
             'encoding' => 'utf8',
             'timezone' => 'UTC',
             'flags' => [],
